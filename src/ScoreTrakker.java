06f24f714804d95333ad9ad12cd0f11f9a082df7
Lucy y Mahorner <lucymahorner@mymail.mines.edu>
2020-02-11 13:18:09 -0700

    final commit

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -4,8 +4,11 @@ import java.util.*;
 import java.io.*;
 
 public class ScoreTrakker{
+	//Declare variables
 	private ArrayList<Student> students;
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
 	
+	// Method reads in text file to variables
 	public void loadDataFromFile(String fileName) throws FileNotFoundException{
 		students = new ArrayList<Student>();
 		FileReader reader = new FileReader(fileName);
@@ -13,12 +16,18 @@ public class ScoreTrakker{
 		while(in.hasNextLine()){
 			String name = in.nextLine();
 			String score = in.nextLine();
-			int numScore = Integer.parseInt(score);
-			Student readIn = new Student(name, numScore);
-			students.add(readIn);
+			try {
+				int numScore = Integer.parseInt(score);
+				Student readIn = new Student(name, numScore);
+				students.add(readIn);
+		    }
+		    catch (NumberFormatException e) { // If not an integer score, print error message
+		       	System.out.println(name + " " + score + " is not a valid score");
+		    }
 		}
 	}
 	
+	// Method prints out the students
 	public void printInOrder(){
 		Collections.sort(students);
 		for(Student student: students){
@@ -26,11 +35,20 @@ public class ScoreTrakker{
 		}
 	}
 	
+	// Reads in file names to loadDataFromFile function
 	public void processFiles() throws FileNotFoundException{
-		loadDataFromFile("scores.txt");
-		printInOrder();
+		for(String fileName: files){
+			try{
+				loadDataFromFile(fileName);
+				printInOrder();
+			}
+			catch (FileNotFoundException e){ // Print error if file does not exist
+				System.out.println(fileName + " does not exist.");
+			}
+		}
 	}
 	
+	//Call processFiles
 	public static void main(String args[]) throws FileNotFoundException{
 		ScoreTrakker tracker = new ScoreTrakker();
 		tracker.processFiles();
